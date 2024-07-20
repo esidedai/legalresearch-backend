87e8c399b09f35aac82568de6a84d0b6a4060f8c
esidedai <165277697+esidedai@users.noreply.github.com>
2024-07-21 00:50:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 const allowedOrigins = [
-    'https://tradeideas.vercel.app',
+    'https://legalresearch.vercel.app',
     'http://localhost:3001'
 ];
 
